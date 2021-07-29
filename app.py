095d1f837ceae19053c2216898555e873a234cd4
Matt Dawson <mattdawson18@gmail.com>
2021-07-29 17:14:22 +0100

    Database and table created using SQLAlchemy

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+from dotenv import load_dotenv
+load_dotenv()
+import os
+password = os.environ.get("db_pass")
 
 app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:%s@localhost/height_collector' % password
+db=SQLAlchemy(app)
+class Data(db.Model):
+    __tablename__='data'
+    id=db.Column(db.Integer, primary_key=True)
+    email=db.Column(db.String(120), unique=True)
+    height=db.Column(db.Integer)
+
+    def __init__(self, email, height):
+        self.email=email
+        self.height=height
 
 @app.route('/')
 #implicitly declares get
@@ -19,3 +36,5 @@ def success():
 if __name__ == '__main__': #checks that the app is running and not being IMPORTED
     app.debug=True
     app.run()
+
+
